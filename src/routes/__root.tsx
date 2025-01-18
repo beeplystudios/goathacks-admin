06141df86682590e7c9781560624378e96bd29eb
awareermine <aramie.ewen@gmail.com>
2025-01-18 11:42:48 -0500

    colors and shpes, etc

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { AuthState } from "../utils/mall";
 import { APIProvider } from "@vis.gl/react-google-maps";
 
@@ -15,7 +15,7 @@ function RootComponent() {
       <APIProvider apiKey={import.meta.env.VITE_GOOGLE_API_KEY}>
         <Outlet />
       </APIProvider>
-      <TanStackRouterDevtools position="bottom-right" />
+      {/* <TanStackRouterDevtools position="bottom-right" /> */}
     </>
   );
 }
--- src/routes/_auth.index.tsx
+++ src/routes/_auth.index.tsx
@@ -1,7 +1,12 @@
 import { createFileRoute, useRouter } from "@tanstack/react-router";
-import { Button } from "../components/ui/button";
 import { useAuthStore } from "../utils/mall";
-import { AdvancedMarker, Map as GoogleMap, Pin, useMap, useMapsLibrary } from "@vis.gl/react-google-maps"
+import {
+  AdvancedMarker,
+  Map as GoogleMap,
+  Pin,
+  useMap,
+  useMapsLibrary,
+} from "@vis.gl/react-google-maps";
 import { useEffect, useState } from "react";
 import { generateRoute, Path, PositionType } from "../utils/algorithm";
 
@@ -18,22 +23,16 @@ function RouteComponent() {
     lng: -71.39225539916762,
   };
 
-  const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+  const colors = ["red", "blue", "purple"];
 
   const [markers, setMarkers] = useState<PositionType[]>([]);
   const [dirServ, setDirServ] = useState<google.maps.DirectionsService>();
   const [distMat, setDistMat] = useState<google.maps.DistanceMatrixService>();
-  const [dirRenderers, setDirRenderers] = useState<google.maps.DirectionsRenderer[]>([]);
-  const [dirRenderer, setDirRenderer] = useState<google.maps.DirectionsRenderer>();
-  const [idxTarget, setIdxTarget] = useState<string>("");
+  const [dirRenderers, setDirRenderers] = useState<
+    google.maps.DirectionsRenderer[]
+  >([]);
   const [path, setPath] = useState<Path>();
-  const [dragging, setDragging] = useState(false);
-
-  // const [startIdx, setStartIdx] = useState<number>();
-  // const [endIdx, setEndIdx] = useState<number>();
   const [selectedIdx, setSelectedIdx] = useState<number | null>(null);
-  // const [dist, setDist] = useState<number>();
-  // const [showDist, setShowDist] = useState(false);
 
   const routes = useMapsLibrary("routes");
   const geometry = useMapsLibrary("geometry");
@@ -43,34 +42,8 @@ function RouteComponent() {
     if (routes && map) {
       setDirServ(new routes.DirectionsService());
       setDistMat(new routes.DistanceMatrixService());
-      setDirRenderer(new routes.DirectionsRenderer({ map, suppressMarkers: true, preserveViewport: true }));
     }
   }, [routes, map]);
-
-  // useEffect(() => {
-  //   if (startIdx != null && endIdx != null && !dragging) {
-  //     dirServ
-  //       ?.route({
-  //         origin: markers[startIdx],
-  //         destination: markers[endIdx],
-  //         travelMode: google.maps.TravelMode.DRIVING,
-  //       })
-  //       .then((resp) => {
-  //         setDist(resp.routes[0].legs[0].distance?.value);
-  //         dirRenderer?.setDirections(resp);
-  //       });
-  //   }
-  // }, [dirServ, markers, startIdx, endIdx, dirRenderers, dragging]);
-
-  // useEffect(() => {
-  //   if (dirRenderer) {
-  //     if (showDist) {
-  //       dirRenderer.setMap(map);
-  //     } else {
-  //       dirRenderer.setMap(null);
-  //     }
-  //   }
-  // }, [showDist, dirRenderer]);
 
   useEffect(() => {
     if (path) {
@@ -79,8 +52,8 @@ function RouteComponent() {
         dirRenderers[i].setOptions({
           polylineOptions: {
             strokeColor: colors[i % colors.length],
-            strokeOpacity: 0.5
-          }
+            strokeOpacity: 0.5,
+          },
         });
       });
     }
@@ -88,46 +61,31 @@ function RouteComponent() {
 
   return (
     <div>
-      Hello "/"!
-      <Button
-        onPress={() => {
-          logOut();
-
-          router.invalidate();
-        }}
-      >
-        Log Out
-      </Button>
       <div className="flex flex-row gap-2">
         <GoogleMap
-          className="rounded-[10px] w-[80%] h-[95vh]"
+          className="w-[80%] h-[100vh]"
           defaultCenter={position}
           defaultZoom={14}
           mapId={import.meta.env.VITE_MAP_ID}
           disableDefaultUI
           disableDoubleClickZoom
           onClick={() => setSelectedIdx(null)}
-          onDblclick={
-            (e) => {
-              if (markers.length >= 1 && routes) {
-                setDirRenderers([...dirRenderers, new routes.DirectionsRenderer({ map, suppressMarkers: true, preserveViewport: true })]);
-              }
-              setMarkers(e.detail.latLng ? [...markers, e.detail.latLng] : markers);
+          onDblclick={(e) => {
+            if (markers.length >= 1 && routes) {
+              setDirRenderers([
+                ...dirRenderers,
+                new routes.DirectionsRenderer({
+                  map,
+                  suppressMarkers: true,
+                  preserveViewport: true,
+                }),
+              ]);
             }
-          }
+            setMarkers(
+              e.detail.latLng ? [...markers, e.detail.latLng] : markers
+            );
+          }}
         >
-          {/*avgLatLng !== undefined ?
-            <AdvancedMarker
-              position={avgLatLng}
-            >
-              <Pin
-                background="yellow"
-                borderColor="black"
-              />
-            </AdvancedMarker>
-            :
-            <></>
-          */}
           {markers.map((m, i) => (
             <AdvancedMarker
               position={m}
@@ -135,14 +93,7 @@ function RouteComponent() {
               draggable
               onClick={() => {
                 setSelectedIdx(i);
-                // if (idxTarget === "start") {
-                //   setStartIdx(i);
-                // } else if (idxTarget === "end") {
-                //   setEndIdx(i);
-                // }
               }}
-              onDragStart={() => setDragging(true)}
-              onDragEnd={() => setDragging(false)}
               onDrag={(e) => {
                 markers[i] = {
                   lat: e.latLng!.lat(),
@@ -159,25 +110,50 @@ function RouteComponent() {
             </AdvancedMarker>
           ))}
         </GoogleMap>
-        <div className="flex flex-col gap-2">
-          <p>{`Lat: ${selectedIdx !== null ? markers[selectedIdx].lat : "None"}`}</p>
-          <p>{`Lng: ${selectedIdx !== null ? markers[selectedIdx].lng : "None"}`}</p>
+        <div className="flex flex-col h-screen w-[20%] gap-5 p-3 items-center">
           <button
-            disabled={selectedIdx === null}
-            onClick={() => {
-              setMarkers([...markers.slice(0, selectedIdx!), ...markers.slice(selectedIdx! + 1, markers.length)])
-              setSelectedIdx(null);
-            }}
-          >
-            Delete
-          </button>
-          <button
+            className="bg-green-800 p-2 rounded-md hover:bg-green-900 transition-colors"
             disabled={!dirServ || !geometry || !distMat}
             onClick={async () => {
-              setPath(await generateRoute(dirServ!, distMat!, geometry!, markers));
+              setPath(
+                await generateRoute(dirServ!, distMat!, geometry!, markers)
+              );
             }}
           >
             Generate Route
+          </button>
+
+          <div className="flex-1" />
+
+          {selectedIdx !== null && (
+            <div className="outline outline-2 rounded-md outline-gray-600 p-2 w-full">
+              <p>{`Lat: ${selectedIdx !== null ? markers[selectedIdx].lat : "None"}`}</p>
+              <p>{`Lng: ${selectedIdx !== null ? markers[selectedIdx].lng : "None"}`}</p>
+              <center>
+                <button
+                  className="mt-2 bg-red-800 p-2 rounded-md hover:bg-red-900 transition-colors"
+                  disabled={selectedIdx === null}
+                  onClick={() => {
+                    setMarkers([
+                      ...markers.slice(0, selectedIdx!),
+                      ...markers.slice(selectedIdx! + 1, markers.length),
+                    ]);
+                    setSelectedIdx(null);
+                  }}
+                >
+                  Delete Selected
+                </button>
+              </center>
+            </div>
+          )}
+
+          <button
+            onClick={() => {
+              logOut();
+              router.invalidate();
+            }}
+          >
+            Log Out
           </button>
         </div>
       </div>
